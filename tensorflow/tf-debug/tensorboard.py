adc1f9fc8cfe7cf8d953d6aa7c2aa8b8f82f1e21
jaassoon <linglijunmail@gmail.com>
2018-12-07 00:57:55 +0000

    Update tensorboard.py

--- tensorflow/tf-debug/tensorboard.py
+++ tensorflow/tf-debug/tensorboard.py
@@ -3,10 +3,10 @@ import tensorflow as tf
 from keras.callbacks import TensorBoard
 from keras.layers import Input, Dense
 from keras.models import Model
-
-import tensorflow as tf
+# Just disables the warning, doesn't enable AVX/FMA
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 from tensorflow.python import debug as tf_debug
-import keras
 
 def write_log(callback, names, logs, batch_no):
     for name, value in zip(names, logs):
@@ -26,14 +26,12 @@ log_path = './logs'
 callback = TensorBoard(log_path)
 callback.set_model(model)
 train_names = ['train_loss' ]
-for batch_no in range(10):
+for batch_no in range(3):
     X_train, Y_train = np.random.rand(32, 3), np.random.rand(32, 1)
     logs = model.train_on_batch(X_train, Y_train)
     print(logs)
-    logstmp=[]
-    logstmp.append(logs)
-    write_log(callback, train_names, logstmp, batch_no)
+    write_log(callback, train_names, [logs], batch_no)
     
-    if batch_no % 10 == 0:
+    if batch_no % 1 == 0:
         X_val, Y_val = np.random.rand(32, 3), np.random.rand(32, 1)
         logs = model.train_on_batch(X_val, Y_val)
